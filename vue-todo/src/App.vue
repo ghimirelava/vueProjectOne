0cb0640bbb0603bccd1823c6fee7c62bcf23ddc1
ghimirelava <lghimire@usfca.edu>
2025-01-05 13:54:27 -0800

    script setup example

---
+++ vue-todo/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1> My name is {{ name }}, I am {{ age }} years old.</h1>
+</template>
+
+<!-- script setup pattern example -->
+<script setup>
+
+//ref function that creates a reactive reference to a val
+import { ref } from 'vue'
+
+const name = ref('Lava')
+const age = ref(24)
+
+</script>
